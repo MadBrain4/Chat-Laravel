0e55f89fd27931b8403e85d13ccea3f1920c87ad
José López <Josevld412@gmail.com>
2023-11-13 21:47:33 -0400

    Maquetado Inicial

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'home')->name('home');
+
+//Auth
+Route::view('/register', 'auth.register')->name('register');
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register_action');
+Route::post('/login', [AuthController::class, 'login'])->name('login_action');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
